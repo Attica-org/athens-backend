6cc7bece879234a670b0c9aaf92a28305e210c22
Mint <audwl03071@gmail.com>
2024-05-05 18:46:56 +0900

    [#4] feat: Spring Security를 활용한 JWT 기반 회원, 비회원 인증 로직 구현
    - JWTUtil: JWT 토큰 생성, 조회 로직을 포함한다.
    - JWTFilter : jwt 토큰을 검증하여 유효할 경우 회원, 비회원 객체를 생성하여 SecurityContextHolder에 인증된 사용자 정보를 저장한다. LoginFilter 전에 수행된다.
    
    - LoginFilter: 인증이 필요한 url일 경우 authenticationManager로 인증 요청을 보내며, 인증 성공시 jwt 토큰을 발급하고 실패할 경우 401 상태 코드를 반환한다.
    
    - CustomUserDetailsService : user를 DB에서 조회하여 존재하는 유저일 경우 CustomUserDetails를 생성하여 반환한다.
    - CustomUserDetails : UserDetails 구현체이며 추상 클래스 BaseUser에 의존한다.
    
    - SecurityConfig : 기본 Spring Security 설정
    - CorsMvcConfig : Cors 설정

---
+++ src/main/java/com/attica/athens/config/SecurityConfig.java
@@ -0,0 +1,107 @@
+package com.attica.athens.config;
+
+import com.attica.athens.security.JWTFilter;
+import com.attica.athens.security.JWTUtil;
+import com.attica.athens.security.LoginFilter;
+import java.util.Collections;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    public static final String[] PUBLIC_URLS = {
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/swagger-resources/**",
+            "/ws/**"
+    };
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+
+    @Value("${frontend.local}")
+    private String frontLocal;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((cors) -> cors
+                        .configurationSource(request -> {
+
+                            CorsConfiguration configuration = new CorsConfiguration();
+
+                            configuration.setAllowedOrigins(Collections.singletonList(frontLocal));
+                            configuration.setAllowedMethods(Collections.singletonList("*"));
+                            configuration.setAllowCredentials(true);
+                            configuration.setAllowedHeaders(Collections.singletonList("*"));
+                            configuration.setMaxAge(3600L);
+
+                            configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                            return configuration;
+                        }));
+
+        // csrf disable
+        http
+                .csrf(AbstractHttpConfigurer::disable);
+
+        // Form 로그인 방식 disable
+        http
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        // http basic 인증 방식 disable
+        http
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        // 경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(("/api/v1/temp-user/test")).hasRole("TEMP_USER")
+                        .anyRequest().permitAll()
+                );
+
+        // LoginFilter 등록 (/login시 동작)
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil),
+                        UsernamePasswordAuthenticationFilter.class);
+
+        // JWTFilter 등록 (모든 요청에 대해 동작)
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+
+        // 세션 설정 (statelss하도록)
+        http
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/attica/athens/security/JWTFilter.java
@@ -0,0 +1,95 @@
+package com.attica.athens.security;
+
+import com.attica.athens.config.SecurityConfig;
+import com.attica.athens.user.domain.TempUser;
+import com.attica.athens.user.domain.User;
+import com.attica.athens.user.domain.UserRole;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+public class JWTFilter extends OncePerRequestFilter {
+
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+
+    private final JWTUtil jwtUtil;
+
+    public JWTFilter(JWTUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (isNonProtectedUrl(request)) {
+            return;
+        }
+
+        String authorization = request.getHeader(AUTHORIZATION);
+
+        if (authorization == null || !authorization.startsWith(BEARER)) {
+            logger.debug("Token is null");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        String token = authorization.split(" ")[1];
+
+        if (jwtUtil.isExpired(token)) {
+            logger.debug("Token is expired");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        String id = jwtUtil.getId(token);
+        String role = jwtUtil.getRole(token);
+
+        if (role.equals(UserRole.ROLE_TEMP_USER.name())) {
+            TempUser tempUser = new TempUser(id);
+            CustomUserDetails customUserDetails = new CustomUserDetails(tempUser);
+
+            Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
+                    customUserDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        User user = new User(id, "fakeUsername", "fakePassword");
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null,
+                customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private boolean isNonProtectedUrl(HttpServletRequest request) {
+
+        for (String urlPattern : SecurityConfig.PUBLIC_URLS) {
+            AntPathRequestMatcher matcher = new AntPathRequestMatcher(urlPattern);
+
+            if (matcher.matches(request)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
