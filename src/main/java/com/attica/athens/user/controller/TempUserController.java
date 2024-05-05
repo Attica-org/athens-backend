a44173a8feedb73074841f3d2ac72120408930e1
Mint <audwl03071@gmail.com>
2024-05-05 19:39:39 +0900

    [#4] refactor: 경로별 인가 작업 수정
    - 기본적으로 모든 요청을 인증하도록 하고, public url만 공개하도록 한다.
    - 비회원 중 권한이 필요한 기능에만 hasRole을 이용해 인가 작업을 수행한다.
    
    - test 엔드포인트를 TestController를 따로 생성하여 분리한다.
    - 문자열은 상수로 리팩토링한다.

--- src/main/java/com/attica/athens/user/controller/TempUserController.java
+++ src/main/java/com/attica/athens/user/controller/TempUserController.java
@@ -3,7 +3,6 @@ package com.attica.athens.user.controller;
 import com.attica.athens.user.service.TempUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TempUserController {
 
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String BEARER = "Bearer ";
+
     private final TempUserService tempUserService;
 
     @PostMapping
@@ -20,12 +22,6 @@ public class TempUserController {
 
         String token = tempUserService.createTempUser();
 
-        return ResponseEntity.ok().header("Authorization", "Bearer " + token).build();
-    }
-
-    @GetMapping("/test")
-    public String test() {
-
-        return "Test Controller";
+        return ResponseEntity.ok().header(AUTHORIZATION, BEARER + token).build();
     }
 }
