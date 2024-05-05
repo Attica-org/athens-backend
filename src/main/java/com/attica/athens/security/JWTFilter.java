048f07f897139b6121be2d753600bee668c11601
Mint <audwl03071@gmail.com>
2024-05-05 19:36:43 +0900

    [#4] fix: 필터 체인 제어 넘기도록 수정
    - public url일 경우 다음 필터 체인으로 넘기지 않으면 서비스 레이어가 실행되지 않는다.
    - 다음 필터 체인으로 넘겨 서비스가 정상적으로 실행되도록 수정한다.

--- src/main/java/com/attica/athens/security/JWTFilter.java
+++ src/main/java/com/attica/athens/security/JWTFilter.java
@@ -33,6 +33,8 @@ public class JWTFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         if (isNonProtectedUrl(request)) {
+            filterChain.doFilter(request, response);
+
             return;
         }
 
