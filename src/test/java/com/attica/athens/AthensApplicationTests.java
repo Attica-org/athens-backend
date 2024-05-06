57b3d568a4a69d10db0b8c8bdb4b01f707a8d195
Big-Cir97 <eodnjs01477@gmail.com>
2024-05-06 19:49:07 +0900

    chore: ci script 추가

--- src/test/java/com/attica/athens/AthensApplicationTests.java
+++ src/test/java/com/attica/athens/AthensApplicationTests.java
@@ -3,7 +3,7 @@ package com.attica.athens;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = AthensApplicationTests.class)
 class AthensApplicationTests {
 
 	@Test
