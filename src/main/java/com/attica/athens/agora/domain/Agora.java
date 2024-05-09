d7e27b8c5027428db41a3c91db420963dabbea6c
Big-Cir97 <eodnjs01477@gmail.com>
2024-05-09 15:19:25 +0900

    feat: agora entity 선언

---
+++ src/main/java/com/attica/athens/agora/domain/Agora.java
@@ -0,0 +1,34 @@
+package com.attica.athens.agora.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "agora")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Agora {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "code")
+    private Category code;
+}
