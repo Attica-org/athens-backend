b8cc158f817d5e9c85063e1f2c5ee8d2451bc28e
Big-Cir97 <eodnjs01477@gmail.com>
2024-05-09 15:19:39 +0900

    feat: agora repository 선언

---
+++ src/main/java/com/attica/athens/agora/repository/AgoraRepository.java
@@ -0,0 +1,7 @@
+package com.attica.athens.agora.repository;
+
+import com.attica.athens.agora.domain.Agora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgoraRepository extends JpaRepository<Long, Agora> {
+}
