89a5a3463fa3fed64e3bf73a2ce461225e889a1f
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-11 02:11:40 +0900

    Revert "feat: category repository 선언"
    This reverts commit fd63e59f46d32867f24314c9af8872a37e52d8fa.

--- src/main/java/com/attica/athens/agora/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.attica.athens.agora.repository;
-
-import com.attica.athens.agora.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<String, Category> {
-}
