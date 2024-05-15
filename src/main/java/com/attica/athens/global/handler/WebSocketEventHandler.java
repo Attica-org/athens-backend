2cd8c7fe69ee4c13fabd8e110f95b2c4e3d576ff
Mint <audwl03071@gmail.com>
2024-05-15 15:10:08 +0900

    refactor: 웹소켓 이벤트 핸들러 리팩토링
    - 이벤트 객체에서 세션 정보를 가져올 수 없는 문제가 있어서 제거한다.

--- src/main/java/com/attica/athens/global/handler/WebSocketEventHandler.java
+++ src/main/java/com/attica/athens/global/handler/WebSocketEventHandler.java
@@ -1,6 +1,5 @@
 package com.attica.athens.global.handler;
 
-import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -13,27 +12,27 @@ import org.springframework.web.socket.messaging.SessionUnsubscribeEvent;
 public class WebSocketEventHandler {
 
     @EventListener
-    public void handleWebSocketSessionConnected(SessionConnectedEvent session) {
-        log.info("WebSocket Connected: " + Objects.requireNonNull(session.getUser()).getName());
+    public void handleWebSocketSessionConnected(SessionConnectedEvent event) {
+        log.info("WebSocket Connected: ");
     }
 
     @EventListener
     public void handleWebSocketSessionDisconnected(SessionUnsubscribeEvent session) {
-        log.info("WebSocket Disconnected: " + Objects.requireNonNull(session.getUser()).getName());
+        log.info("WebSocket Disconnected: ");
     }
 
     @EventListener
     public void handleWebSocketSessionSubscribe(SessionSubscribeEvent session) {
-        log.info("WebSocket Subscribe: " + Objects.requireNonNull(session.getUser()).getName());
+        log.info("WebSocket Subscribe: ");
     }
 
     @EventListener
     public void handleWebSocketSessionUnsubscribe(SessionUnsubscribeEvent session) {
-        log.info("WebSocket Unsubscribe: " + Objects.requireNonNull(session.getUser()).getName());
+        log.info("WebSocket Unsubscribe: ");
     }
 
     @EventListener
     public void handleWebSocketSessionConnectedEventListener(SessionConnectedEvent session) {
-        log.info("WebSocket Connected: " + Objects.requireNonNull(session.getUser()).getName());
+        log.info("WebSocket Connected: ");
     }
 }
