46c2ce564f5b06046944143041968a6789ce4fa2
Mint <audwl03071@gmail.com>
2024-05-19 21:01:19 +0900

    feat: 웹소켓 세부 설정
    - 스레드 풀 크기 설정
    - 전송시간, 버퍼크기 제한, 메세지 크기 제한
    - sockJS는 APIC 테스트를 위해 비활성화

--- src/main/java/com/attica/athens/global/config/WebSocketConfig.java
+++ src/main/java/com/attica/athens/global/config/WebSocketConfig.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -24,16 +27,32 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint(ENDPOINT)
                 .setAllowedOriginPatterns("*");
+//                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker(SIMPLE_BROKER);
         registry.setApplicationDestinationPrefixes(PUBLISH);
+        registry.enableSimpleBroker(SIMPLE_BROKER)
+                .setTaskScheduler(taskScheduler())
+                .setHeartbeatValue(new long[]{3000L, 3000L});
+    }
+
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(10);
+        taskScheduler.initialize();
+        return taskScheduler;
     }
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
         registration.interceptors(jwtChannelInterceptor);
+    }
+
+    @Override
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setSendTimeLimit(15 * 1000).setSendBufferSizeLimit(512 * 1024);
+        registration.setMessageSizeLimit(128 * 1024);
     }
 }
