51fecfd548efc244d5574f3a3e64c9ad8537165f
Big-Cir97 <eodnjs01477@gmail.com>
2024-05-29 17:48:28 +0900

    [#50] feat: aogra 입장 req, res 객체 추가

---
+++ src/main/java/com/attica/athens/domain/agora/dto/request/AgoraParticipateRequest.java
@@ -0,0 +1,20 @@
+package com.attica.athens.domain.agora.dto.request;
+
+import com.attica.athens.domain.agoraUser.domain.AgoraUserType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
+public record AgoraParticipateRequest(
+    @NotBlank
+    String nickname,
+    @NotNull
+    Integer photoNum,
+    @Pattern(regexp = "^(PROS|CONS|OBSERVER)$", message = "허용되지 않는 role 입니다.")
+    String role
+) {
+
+    public AgoraUserType getAgoraUserType() {
+        return AgoraUserType.valueOf(role);
+    }
+}
