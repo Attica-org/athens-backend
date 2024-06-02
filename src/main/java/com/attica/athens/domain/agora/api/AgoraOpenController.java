7d4cf8ccca1358c826465e79d16c0fcb3f372dd6
Mint <audwl03071@gmail.com>
2024-06-02 23:38:26 +0900

    fix: 아고라 생성 에러 해결
    - createdBy가 저장되지 않는 문제 해결

--- src/main/java/com/attica/athens/domain/agora/api/AgoraOpenController.java
+++ src/main/java/com/attica/athens/domain/agora/api/AgoraOpenController.java
@@ -2,19 +2,15 @@ package com.attica.athens.domain.agora.api;
 
 import com.attica.athens.domain.agora.application.AgoraService;
 import com.attica.athens.domain.agora.dto.SimpleAgoraResult;
-import com.attica.athens.domain.agora.dto.request.AgoraCreateRequest;
 import com.attica.athens.domain.agora.dto.request.SearchCategoryRequest;
 import com.attica.athens.domain.agora.dto.request.SearchKeywordRequest;
 import com.attica.athens.domain.agora.dto.response.AgoraSlice;
-import com.attica.athens.domain.agora.dto.response.CreateAgoraResponse;
 import com.attica.athens.domain.common.ApiResponse;
 import com.attica.athens.domain.common.ApiUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,15 +38,6 @@ public class AgoraOpenController {
     ) {
         AgoraSlice<SimpleAgoraResult> response =
                 agoraService.findAgoraByKeyword(agoraName, new SearchKeywordRequest(request.status(), request.next()));
-
-        return ResponseEntity.ok(ApiUtil.success(response));
-    }
-
-    @PostMapping
-    public ResponseEntity<ApiResponse<?>> createAgora(
-            @RequestBody @Valid AgoraCreateRequest request
-    ) {
-        CreateAgoraResponse response = agoraService.create(request);
 
         return ResponseEntity.ok(ApiUtil.success(response));
     }
