da20b6ee391eba34e868f9f13c51a5e7279f8842
Big-Cir97 <eodnjs01477@gmail.com>
2024-06-06 21:39:37 +0900

    [#79] refactor: agora status 조회 수정

--- src/main/java/com/attica/athens/domain/agora/dto/request/SearchCategoryRequest.java
+++ src/main/java/com/attica/athens/domain/agora/dto/request/SearchCategoryRequest.java
@@ -1,9 +1,12 @@
 package com.attica.athens.domain.agora.dto.request;
 
+import static com.attica.athens.domain.agora.domain.AgoraStatus.CLOSED;
+import static com.attica.athens.domain.agora.domain.AgoraStatus.QUEUED;
+import static com.attica.athens.domain.agora.domain.AgoraStatus.RUNNING;
+
 import com.attica.athens.domain.agora.domain.AgoraStatus;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import java.util.Arrays;
 import java.util.List;
 
 public record SearchCategoryRequest(
@@ -15,8 +18,8 @@ public record SearchCategoryRequest(
 ) {
 
     public List<AgoraStatus> getStatus() {
-        return Arrays.stream(AgoraStatus.values())
-                .filter(agoraStatus -> agoraStatus.getType().equals("active"))
-                .toList();
+        return status.equals(CLOSED.getType()) ?
+                List.of(CLOSED) :
+                List.of(QUEUED, RUNNING);
     }
 }
