fd291c88a38fa853046257767aff10b7991692b0
Mint <audwl03071@gmail.com>
2024-06-06 17:43:22 +0900

    refactor: 채팅 내역 조회 리팩토링
    - 요청에 agoraId가 유효하지 않을 경우 예외 처리
    - LocalDateTime 응답 formatting

--- src/main/java/com/attica/athens/domain/chat/dto/response/GetChatResponse.java
+++ src/main/java/com/attica/athens/domain/chat/dto/response/GetChatResponse.java
@@ -1,10 +1,11 @@
 package com.attica.athens.domain.chat.dto.response;
 
+import static com.attica.athens.global.utils.TimeFormatter.format;
+
 import com.attica.athens.domain.agoraUser.domain.AgoraUser;
 import com.attica.athens.domain.chat.domain.Chat;
 import com.attica.athens.domain.chat.dto.Cursor;
 import com.attica.athens.domain.chat.dto.response.SendChatResponse.UserData;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public record GetChatResponse(List<ChatData> chats, Cursor meta) {
@@ -12,11 +13,11 @@ public record GetChatResponse(List<ChatData> chats, Cursor meta) {
             Long chatId,
             UserData user,
             String content,
-            LocalDateTime createdAt
+            String createdAt
     ) {
         public ChatData(Chat chat, AgoraUser agoraUser) {
             this(chat.getId(), new UserData(agoraUser), chat.getContent(),
-                    chat.getCreatedAt());
+                    format(chat.getCreatedAt()));
         }
     }
 }
