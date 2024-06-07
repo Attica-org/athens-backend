fc49355174275cd303f1d95476e8fc6df0ef2e97
Big-Cir97 <eodnjs01477@gmail.com>
2024-06-07 17:39:31 +0900

    [#79] refactor: 참여자 응답 필드 타입 int 변경

--- src/main/java/com/attica/athens/domain/agora/dto/SimpleParticipants.java
+++ src/main/java/com/attica/athens/domain/agora/dto/SimpleParticipants.java
@@ -1,8 +1,12 @@
 package com.attica.athens.domain.agora.dto;
 
 public record SimpleParticipants(
-        Long pros,
-        Long cons,
-        Long observer
+        int pros,
+        int cons,
+        int observer
 ) {
+
+    public SimpleParticipants(Long pros, Long cons, Long observer) {
+        this(pros.intValue(), cons.intValue(), observer.intValue());
+    }
 }
