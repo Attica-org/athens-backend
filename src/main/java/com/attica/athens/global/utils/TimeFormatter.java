18ba0c1302f8e0c52053ab918990f1aaa72e1a4a
Mint <audwl03071@gmail.com>
2024-06-07 14:50:30 +0900

    refactor: Lombok을 이용해 private 생성자 생성

--- src/main/java/com/attica/athens/global/utils/TimeFormatter.java
+++ src/main/java/com/attica/athens/global/utils/TimeFormatter.java
@@ -2,14 +2,13 @@ package com.attica.athens.global.utils;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class TimeFormatter {
 
     private static final DateTimeFormatter FORMATTER =
             DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-    private TimeFormatter() {
-    }
 
     public static String format(LocalDateTime dateTime) {
         return dateTime.format(FORMATTER);
