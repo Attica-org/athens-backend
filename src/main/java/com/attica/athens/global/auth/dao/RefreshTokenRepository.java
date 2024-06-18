3051a2df7948562c938b7632fd470445c086c114
yoobinmion <yyb400@ajou.ac.kr>
2024-06-18 14:15:58 +0900

    [#98]feat: refresh token 만료 시 자정마다 db에서 삭제

--- src/main/java/com/attica/athens/global/auth/dao/RefreshTokenRepository.java
+++ src/main/java/com/attica/athens/global/auth/dao/RefreshTokenRepository.java
@@ -1,6 +1,9 @@
 package com.attica.athens.global.auth.dao;
 
 import com.attica.athens.global.auth.domain.RefreshToken;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,5 +14,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     @Transactional
     void deleteByRefresh(String refresh);
+
+    List<RefreshToken> findByExpirationBefore(Date date);
 
 }
