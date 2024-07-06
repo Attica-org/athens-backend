4f0562af48d761c2c14b4a663f30c69b10e5c4e6
Mint <audwl03071@gmail.com>
2024-07-06 19:01:24 +0900

    test: ChatContent 테스트 작성

---
+++ src/test/java/com/attica/athens/domain/chat/domain/ChatContentTest.java
@@ -0,0 +1,59 @@
+package com.attica.athens.domain.chat.domain;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
+
+import com.attica.athens.domain.chat.exception.ContentEmptyException;
+import com.attica.athens.domain.chat.exception.ContentExceedException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("채팅 내용 테스트")
+class ChatContentTest {
+
+    @Nested
+    @DisplayName("채팅 내용 생성 테스트")
+    public class ChatContentCreationTest {
+
+        @Test
+        @DisplayName("채팅 내용을 생성한다")
+        public void givenContent_whenCreateChatContent_thenChatContentIsCreated() {
+            // Given
+            String content = "안녕";
+
+            // When
+            ChatContent chatContent = new ChatContent(content);
+
+            // Then
+            then(chatContent).extracting("content").isEqualTo(content);
+        }
+
+        @Test
+        @DisplayName("채팅 내용이 없으면 예외를 발생한다")
+        public void givenNullContent_whenCreateChatContent_thenThrowException() {
+            // When & Then
+            thenThrownBy(() -> new ChatContent(null))
+                    .isInstanceOf(NullPointerException.class)
+                    .hasMessage("content must not be null");
+        }
+
+        @Test
+        @DisplayName("채팅 내용이 빈 문자열이면 예외를 발생한다")
+        public void givenEmptyContent_whenCreateChatContent_thenThrowException() {
+            // When & Then
+            thenThrownBy(() -> new ChatContent(""))
+                    .isInstanceOf(ContentEmptyException.class)
+                    .hasMessage("content must not be empty");
+        }
+
+        @Test
+        @DisplayName("채팅 내용이 10000자를 초과하면 예외를 발생한다")
+        public void givenLongContent_whenCreateChatContent_thenThrowException() {
+            // When & Then
+            thenThrownBy(() -> new ChatContent("a".repeat(10001)))
+                    .isInstanceOf(ContentExceedException.class)
+                    .hasMessage("content length exceeds maximum limit of 10000 characters");
+        }
+    }
+}
