3356ea2ee7eecf609ab4edc7b7a884762ea5165c
Mint <audwl03071@gmail.com>
2024-07-07 17:32:08 +0900

    test: 채팅방 참여자 조회 테스트 작성

--- src/test/java/com/attica/athens/domain/chat/ChatOpenApiIntegrationTest.java
+++ src/test/java/com/attica/athens/domain/chat/ChatOpenApiIntegrationTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.ResultActions;
 
 @DisplayName("채팅 오픈 API 통합 테스트")
@@ -19,13 +20,13 @@ public class ChatOpenApiIntegrationTest extends IntegrationTestSupport {
     class GetChatHistoryTest {
 
         @Test
-        @DisplayName("채팅방 내역을 조회한다")
+        @DisplayName("채팅 내역을 조회한다")
         void getChatHistory() throws Exception {
             // Given
 
             // When
             final ResultActions result = mockMvc.perform(
-                    get("/api/v1/open/agoras/1/chats", API_V1, 1)
+                    get("/{apiVersion}/open/agoras/{agoraId}/chats", API_V1, 1)
                             .contentType(MediaType.APPLICATION_JSON)
 
             );
@@ -36,11 +37,44 @@ public class ChatOpenApiIntegrationTest extends IntegrationTestSupport {
                             jsonPath("$.success").value(true),
                             jsonPath("$.error").doesNotExist(),
                             jsonPath("$.response.chats").isArray(),
-                            jsonPath("$.response.chats.length()").value(3),
+                            jsonPath("$.response.chats.length()").value(4),
                             jsonPath("$.response.chats[0].chatId").exists(),
                             jsonPath("$.response.chats[0].user.nickname").isString(),
                             jsonPath("$.response.chats[0].content").isString(),
                             jsonPath("$.response.meta.effectiveSize").value(10)
+                    );
+        }
+    }
+
+    @Nested
+    @DisplayName("채팅방 참여자 조회 테스트")
+    class GetChatParticipantsTest {
+
+        @Test
+        @DisplayName("채팅방 참여자를 조회한다")
+        @Sql("/sql/update-agora-participants.sql")
+        void getChatParticipants() throws Exception {
+            // Given
+
+            // When
+            final ResultActions result = mockMvc.perform(
+                    get("/{apiVersion}/open/agoras/{agoraId}/users", API_V1, 1)
+                            .contentType(MediaType.APPLICATION_JSON)
+
+            );
+
+            // Then
+            result.andExpect(status().isOk())
+                    .andExpectAll(
+                            jsonPath("$.success").value(true),
+                            jsonPath("$.error").doesNotExist(),
+                            jsonPath("$.response.agoraId").value(1),
+                            jsonPath("$.response.participants").isArray(),
+                            jsonPath("$.response.participants.length()").value(5),
+                            jsonPath("$.response.participants[0].id").exists(),
+                            jsonPath("$.response.participants[0].nickname").isString(),
+                            jsonPath("$.response.participants[0].photoNumber").isNumber(),
+                            jsonPath("$.response.participants[0].type").isString()
                     );
         }
     }
