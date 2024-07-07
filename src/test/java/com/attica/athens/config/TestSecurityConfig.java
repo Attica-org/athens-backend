d297e8694e32b35da59066378ab0508d0826d8b6
Mint <audwl03071@gmail.com>
2024-07-07 22:32:33 +0900

    docs: Test용 Security Config에 주석 추가

--- src/test/java/com/attica/athens/config/TestSecurityConfig.java
+++ src/test/java/com/attica/athens/config/TestSecurityConfig.java
@@ -15,8 +15,12 @@ public class TestSecurityConfig {
 
         private final Map<String, CustomUserDetails> customUserDetailsMap = new HashMap<>() {
             {
+                // agoraId:1 (Queued), Pros
                 put("EnvironmentalActivist", new CustomUserDetails(1L, "password", "ROLE_USER"));
-                put("EconomistPro", new CustomUserDetails(7L, "password", "ROLE_USER"));
+                // agoraId:1 (Queued), Observer
+                put("PolicyExpert", new CustomUserDetails(3L, "password", "ROLE_USER"));
+                // agoraId:2 (Running), Pros
+                put("TeacherUnion", new CustomUserDetails(4L, "password", "ROLE_USER"));
             }
         };
 
