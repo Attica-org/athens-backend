2986cd81b4c6583266a96595f84ac49737b6c386
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-09 15:26:29 +0900

    메타정보 조회 websocket 테스트 코드 작성 (#150)
    * refactor: 웹소켓 메세지 byte로 받도록 수정
    - 웹소켓 메세지는 길면 chunk로 나누어서 오기 때문에 byte[]로 받아야 에러 없이 잘 받을 수 있다.
    
    * comment: bdd 주석 소문자로 변경
    
    * test: 메타 정보 조회 테스트 작성
    
    * refactor: 필요없는 타입 매개변수 제거

--- src/test/java/com/attica/athens/domain/chat/ChatWebSocketApiIntegrationTest.java
+++ src/test/java/com/attica/athens/domain/chat/ChatWebSocketApiIntegrationTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.attica.athens.domain.chat.domain.ChatType;
 import com.attica.athens.domain.chat.dto.request.SendChatRequest;
-import com.attica.athens.domain.chat.dto.response.ErrorResponse;
-import com.attica.athens.domain.chat.dto.response.SendChatResponse;
 import com.attica.athens.support.WebSocketIntegrationTestSupport;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -41,18 +39,17 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("유효한 메시지를 전송하면 성공적으로 채팅이 전송된다")
         @Sql("/sql/send-valid-chat.sql")
         void 성공_채팅생성_유효한파라미터사용() throws Exception {
-            // Given
+            // given
             Long agoraId = 1L;
             String url = CHAT_TOPIC_URL.replace("{agoraId}", agoraId.toString());
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(url, "EnvironmentalActivist", agoraId, resultFuture,
-                    SendChatResponse.class);
+            StompSession session = connectAndSubscribe(url, "EnvironmentalActivist", agoraId, resultFuture);
 
-            // When
+            // when
             SendChatRequest chatRequest = new SendChatRequest(ChatType.CHAT, "안녕하세요.");
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertThat(result).contains("안녕하세요.");
         }
@@ -61,23 +58,22 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("10000자를 초과하는 메시지를 작성하면 에러를 반환한다")
         @Sql("/sql/send-valid-chat.sql")
         void 실패_채팅전송_메시지길이초과() throws Exception {
-            // Given
+            // given
             Long agoraId = 1L;
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture,
-                    ErrorResponse.class);
+            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture);
 
-            // When
+            // when
             String longMessage = "a".repeat(10001);
             SendChatRequest chatRequest = new SendChatRequest(ChatType.CHAT, longMessage);
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertAll(() -> {
                 assertThat(result).contains("ERROR");
                 assertThat(result).contains("1001");
-                assertThat(result).contains("message=Content length exceeds maximum limit of 10000 characters");
+                assertThat(result).contains("Content length exceeds maximum limit of 10000 characters");
             });
         }
 
@@ -85,17 +81,16 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("type을 입력하지않으면 에러를 반환한다")
         @Sql("/sql/send-valid-chat.sql")
         void 실패_채팅전송_type미입력() throws Exception {
-            // Given
+            // given
             Long agoraId = 1L;
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture,
-                    ErrorResponse.class);
+            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture);
 
-            // When
+            // when
             SendChatRequest chatRequest = new SendChatRequest(null, "안녕하세요.");
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertAll(() -> {
                 assertThat(result).contains("ERROR");
@@ -108,17 +103,16 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("빈 메세지를 입력하면 에러를 반환한다")
         @Sql("/sql/send-valid-chat.sql")
         void 실패_채팅전송_빈메세지() throws Exception {
-            // Given
+            // given
             Long agoraId = 1L;
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture,
-                    ErrorResponse.class);
+            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture);
 
-            // When
+            // when
             SendChatRequest chatRequest = new SendChatRequest(ChatType.CHAT, "");
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertAll(() -> {
                 assertThat(result).contains("ERROR");
@@ -131,17 +125,16 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("존재하지 않는 아고라 id를 입력하면 에러를 반환한다")
         @Sql("/sql/send-valid-chat.sql")
         void 실패_채팅전송_존재하지않은AgoraId() throws Exception {
-            // Given
+            // given
             Long agoraId = 9999L;
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture,
-                    ErrorResponse.class);
+            StompSession session = connectAndSubscribe(ERROR_URL, "EnvironmentalActivist", agoraId, resultFuture);
 
-            // When
+            // when
             SendChatRequest chatRequest = new SendChatRequest(ChatType.CHAT, "안녕하세요.");
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertAll(() -> {
                 assertThat(result).contains("ERROR");
@@ -154,17 +147,16 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("유저가 아고라에 참여하지 않은 경우 에러를 반환한다")
         @Sql("/sql/send-valid-chat.sql")
         void 실패_채팅전송_비참여유저() throws Exception {
-            // Given
+            // given
             Long agoraId = 1L;
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(ERROR_URL, "TeacherUnion", agoraId, resultFuture,
-                    ErrorResponse.class);
+            StompSession session = connectAndSubscribe(ERROR_URL, "TeacherUnion", agoraId, resultFuture);
 
-            // When
+            // when
             SendChatRequest chatRequest = new SendChatRequest(ChatType.CHAT, "안녕하세요.");
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertAll(() -> {
                 assertThat(result).contains("ERROR");
@@ -177,17 +169,16 @@ class ChatWebSocketApiIntegrationTest extends WebSocketIntegrationTestSupport {
         @DisplayName("유저가 관찰자일 경우 에러를 반환한다")
         @Sql("/sql/send-valid-chat.sql")
         void 실패_채팅전송_관찰자() throws Exception {
-            // Given
+            // given
             Long agoraId = 1L;
             CompletableFuture<String> resultFuture = new CompletableFuture<>();
-            StompSession session = connectAndSubscribe(ERROR_URL, "PolicyExpert", agoraId, resultFuture,
-                    ErrorResponse.class);
+            StompSession session = connectAndSubscribe(ERROR_URL, "PolicyExpert", agoraId, resultFuture);
 
-            // When
+            // when
             SendChatRequest chatRequest = new SendChatRequest(ChatType.CHAT, "안녕하세요.");
             session.send(CHAT_APP_URL.replace("{agoraId}", agoraId.toString()), chatRequest);
 
-            // Then
+            // then
             String result = resultFuture.get(10, TimeUnit.SECONDS);
             assertAll(() -> {
                 assertThat(result).contains("ERROR");
