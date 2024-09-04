a238467f85e29bae3bf151129b914fde18d8b2c8
dae won <eodnjs01477@gmail.com>
2024-09-04 11:23:35 +0900

    [#169] feat: s3 라이브러리 및 설정 추가

---
+++ src/main/java/com/attica/athens/global/config/S3Config.java
@@ -0,0 +1,30 @@
+package com.attica.athens.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.access-key}")
+    private String accessKey;
+
+    @Value("${aws.s3.secret-key}")
+    private String secretKey;
+
+    @Value("${aws.s3.region.static}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3client() {
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .withRegion(region)
+                .build();
+    }
+}
