f00f5aa1c07a70afbd8ee138ddf0d05a0cbf220e
Mint <audwl03071@gmail.com>
2024-09-24 21:43:45 +0900

    test: JWtUtil 테스트 수정 - 통합 테스트 지원 클래스를 상속받아 테스트가 실패없이 진행되도록 한다.

--- src/test/java/com/attica/athens/global/auth/JwtUtilsTest.java
+++ src/test/java/com/attica/athens/global/auth/JwtUtilsTest.java
@@ -3,15 +3,14 @@ package com.attica.athens.global.auth;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.attica.athens.global.auth.jwt.JwtUtils;
+import com.attica.athens.support.IntegrationTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class JwtUtilsTest {
+class JwtUtilsTest extends IntegrationTestSupport {
 
     @Autowired
     private JwtUtils jwtUtils;
