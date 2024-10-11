443304261ab46a897a884fbe72d09c882eb1acd1
yybmion <yyb400@naver.com>
2024-10-12 04:57:31 +0900

    [#190]refactor: 쓸대없는 ChatRepository 쿼리 삭제

--- src/main/java/com/attica/athens/domain/chat/dao/ChatRepository.java
+++ src/main/java/com/attica/athens/domain/chat/dao/ChatRepository.java
@@ -2,7 +2,6 @@ package com.attica.athens.domain.chat.dao;
 
 
 import com.attica.athens.domain.chat.domain.Chat;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,17 +14,6 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
             "ORDER BY c.id DESC")
     List<Chat> findChatsForAgoraMembers(
             @Param("agoraMemberIds") List<Long> agoraMemberIds,
-            @Param("cursorId") Long cursorId,
-            Pageable pageable
-    );
-
-    @Query("SELECT c FROM Chat c WHERE c.agoraMember.id IN :agoraMemberIds " +
-            "AND c.createdAt >= :joinTime " +
-            "AND (:cursorId IS NULL OR c.id < :cursorId) " +
-            "ORDER BY c.id DESC")
-    List<Chat> findChatsFromJoinTime(
-            @Param("agoraMemberIds") List<Long> agoraMemberIds,
-            @Param("joinTime") LocalDateTime joinTime,
             @Param("cursorId") Long cursorId,
             Pageable pageable
     );
