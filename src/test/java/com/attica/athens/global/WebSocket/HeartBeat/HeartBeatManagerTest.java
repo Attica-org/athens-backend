124638ba5e5c91d849816e27b430508e4a1bd0a9
yybmion <yyb400@naver.com>
2024-10-12 23:47:52 +0900

    [#190]refactor: HeartBeat 단위 테스트 작성

---
+++ src/test/java/com/attica/athens/global/WebSocket/HeartBeat/HeartBeatManagerTest.java
@@ -0,0 +1,84 @@
+package com.attica.athens.global.WebSocket.HeartBeat;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.attica.athens.global.decorator.HeartBeatManager;
+import java.lang.reflect.Field;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("HeartBeat 테스트")
+public class HeartBeatManagerTest {
+
+    @Test
+    @DisplayName("HandleHeartBeat 테스트")
+    void 성공_handleHeartBeat_유효한파라미터() {
+        // given
+        HeartBeatManager manager = new HeartBeatManager();
+        String sessionId = "test-session";
+
+        // when
+        manager.handleHeartbeat(sessionId);
+
+        // then
+        assertNotNull(manager.getLastHeartBeatTimes().get(sessionId));
+        assertTrue(manager.getExpirationTimes().firstEntry().getValue().contains(sessionId));
+    }
+
+    @Nested
+    @DisplayName("재연결 검사")
+    class reconnectTest {
+
+        @Test
+        @DisplayName("재연결 유효성 검사 성공")
+        void 성공_유효성검사_유효한파라미터() throws Exception {
+            // given
+            HeartBeatManager manager = new HeartBeatManager();
+            String sessionId = "test-session";
+
+            // when
+            manager.handleHeartbeat(sessionId);
+
+            //then
+            assertTrue(manager.isReconnectValid(sessionId));
+        }
+
+        @Test
+        @DisplayName("재연결 유효성 검사 실패")
+        void 실패_유효성검사_유효하지않은파라미터() throws Exception {
+            // given
+            HeartBeatManager manager = new HeartBeatManager();
+            String sessionId = "test-session";
+
+            // when
+            Field lastHeartBeatTimesField = HeartBeatManager.class.getDeclaredField("lastHeartBeatTimes");
+            lastHeartBeatTimesField.setAccessible(true);
+            Map<String, LocalDateTime> lastHeartBeatTimes = (Map<String, LocalDateTime>) lastHeartBeatTimesField.get(
+                    manager);
+
+            lastHeartBeatTimes.put(sessionId, LocalDateTime.now().minusSeconds(11));
+
+            // then
+            assertFalse(manager.isReconnectValid(sessionId));
+        }
+    }
+
+    @Test
+    @DisplayName("세션지우기 테스트")
+    void 성공_세션지우기_유효한파라미터() {
+        HeartBeatManager manager = new HeartBeatManager();
+        String sessionId = "test-session";
+
+        manager.handleHeartbeat(sessionId);
+        manager.removeSession(sessionId);
+
+        assertNull(manager.getLastHeartBeatTimes().get(sessionId));
+        assertTrue(manager.getExpirationTimes().isEmpty());
+    }
+}
