527faf9797b434f70aa3c021cabbc9b40b2b0d99
yybmion <yyb400@naver.com>
2025-02-19 15:54:00 +0900

    [#261]refactor: test코드 주석 제거 및 불필요 엔티티 제거

--- src/test/java/com/attica/athens/global/WebSocket/HeartBeat/HeartBeatInterceptorTest.java
+++ src/test/java/com/attica/athens/global/WebSocket/HeartBeat/HeartBeatInterceptorTest.java
@@ -44,7 +44,7 @@ public class HeartBeatInterceptorTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        // 새로운 구조에서는 MessageProcessorFactory를 통해 프로세서를 생성합니다
+
         messageProcessorFactory = new MessageProcessorFactory(authService, heartBeatManager);
         interceptor = new JwtChannelInterceptor(messageProcessorFactory);
     }
@@ -57,18 +57,15 @@ public class HeartBeatInterceptorTest {
         @DisplayName("알맞은 accessor command 하트비트 핸들링 테스트")
         void 성공_하트비트인터셉트_메세지전송직전() {
             // given
-            // 하트비트 메시지 생성
             SimpMessageHeaderAccessor accessor = StompHeaderAccessor.createForHeartbeat();
             accessor.setSessionId("test-session-id");
             byte[] payload = new byte[]{'\n'};
             Message<?> message = MessageBuilder.createMessage(payload, accessor.getMessageHeaders());
 
             // when
-            // 메시지 처리를 수행합니다
             interceptor.preSend(message, null);
 
             // then
-            // 하트비트 매니저가 적절히 호출되었는지 확인합니다
             verify(heartBeatManager, times(1)).handleHeartbeat("test-session-id");
         }
 
@@ -76,17 +73,14 @@ public class HeartBeatInterceptorTest {
         @DisplayName("알맞지 않은 accessor command 하트비트 핸들링 테스트")
         void 실패_하트비트인터셉트_메세지전송직전() {
             // given
-            // 일반 SEND 메시지 생성 (하트비트가 아님)
             StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.SEND);
             accessor.setSessionId("test-session-id");
             Message<?> message = MessageBuilder.createMessage("Some payload", accessor.getMessageHeaders());
 
             // when
-            // 메시지 처리를 수행합니다
             interceptor.preSend(message, null);
 
             // then
-            // 하트비트 처리가 호출되지 않았는지 확인합니다
             verify(heartBeatManager, never()).handleHeartbeat(anyString());
         }
 
@@ -94,15 +88,12 @@ public class HeartBeatInterceptorTest {
         @DisplayName("널 accessor 테스트")
         void 실패_하트비트인터셉트_accessor_null아닌값() {
             // given
-            // null accessor 상황을 시뮬레이션합니다
             when(MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class)).thenReturn(null);
 
             // when
-            // 메시지 처리를 수행합니다
             interceptor.preSend(message, channel);
 
             // then
-            // 하트비트 처리가 호출되지 않았는지 확인합니다
             verify(heartBeatManager, never()).handleHeartbeat(anyString());
         }
     }
