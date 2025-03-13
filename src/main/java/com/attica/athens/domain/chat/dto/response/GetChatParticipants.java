5be1742bde6205bc4a4a37fca46cea57853384df
dae won <eodnjs01477@gmail.com>
2025-03-13 20:23:18 +0900

    feat: thumbnail 응답 추가

--- src/main/java/com/attica/athens/domain/chat/dto/response/GetChatParticipants.java
+++ src/main/java/com/attica/athens/domain/chat/dto/response/GetChatParticipants.java
@@ -5,9 +5,9 @@ import com.attica.athens.domain.chat.dto.response.SendChatResponse.UserData;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record GetChatParticipants(Long agoraId, List<UserData> participants) {
-    public GetChatParticipants(List<AgoraMember> agoraMembers, Long agoraId) {
-        this(agoraId, getParticipants(agoraMembers));
+public record GetChatParticipants(Long agoraId, List<UserData> participants, String agoraThumbnailUrl) {
+    public GetChatParticipants(List<AgoraMember> agoraMembers, Long agoraId, String agoraThumbnailUrl) {
+        this(agoraId, getParticipants(agoraMembers), agoraThumbnailUrl);
     }
 
     private static List<UserData> getParticipants(List<AgoraMember> agoraMembers) {
